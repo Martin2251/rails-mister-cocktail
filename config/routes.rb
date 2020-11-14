2a5fcfc314cd00f6788163727ef23866c0b05f07
martin lowe <martinlowe673@gmail.com>
2020-11-14 19:43:28 +0000

    progress added dose controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+  root to: 'cocktails#index'
+  resources :cocktails, only: [:root, :new, :show, :create] do
+    resources :doses, only: [:new, :create]
+    end
+    resources :doses, only: [:destroy]
+  end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredient_serialized = open(url).read
+ingredient_json = JSON.parse(ingredient_serialized)
+
+10.times do
+  Ingredient.create(
+    name: ingredient_json['drinks'][rand(20)]['strIngredient1']
+  )
+end
